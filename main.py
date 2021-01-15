5088657c096e51d78fdcf7b3af397f707c50123c
Squawylaous <57704977+Squawylaous@users.noreply.github.com>
2021-01-14 22:26:20 -0600

    bux fixes
    various syntax errors fixed. there's still more bugs to fix

--- main.py
+++ main.py
@@ -15,13 +15,13 @@ class chess:
     temphasMoved=False
     tempalive=True
     tempextra=False
-    def reset(self,color,piece,x,y,extra):
+    def reset(self,color,piece,x,y,extra=False):
         self.color=color
         self.piece=piece
         self.x=x
         self.y=y
         self.hasMoved=False
-        slef.alive=True
+        self.alive=True
         self.extra=extra
         self.clone()
     def clone(piece): [piece.tempcolor,piece.temppiece,piece.tempx,piece.tempy,piece.tempalive,piece.temphasMoved,piece.tempextra]=[piece.color,piece.piece,piece.x,piece.y,piece.alive,piece.hasMoved,piece.extra]
@@ -44,10 +44,11 @@ def intToAlphabet(x):
             x%=26**i
     ret+=alpha[x-1]
     return ret
-def alphabetToInt(x):
+def alphabetToInt(y):
     alpha=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
     ret=0
-    x=rev(x)
+    x=""
+    for i in range(len(y)): x=y[i]+x
     for i in range(len(x)):
         ret+=(26**i)*(alpha.index(x[i])+1)
     return ret
@@ -68,11 +69,11 @@ def kingcheck(color):
     global allPieces
     ret=False
     if color=="b":
-        xy=[bking1.x,bking1.y]
+        xy=[allPieces[31].x,allPieces[31].y]
         for piece in allPieces:
             if xy in move(piece): ret=True
     else:
-        xy=[wking1.x,wking1.y]
+        xy=[allPieces[15].x,allPieces[15].y]
         for piece in allPieces:
             if xy in move(piece): ret=True
     return ret
@@ -149,14 +150,17 @@ def linearmoveto(x,y,color,acrossx,acrossy,xlimit=False,ylimit=False,xdirec=0,yd
     return possible
 def checkcheck(piece,possible):
     for i in possible:
-        clone()
+        global allPieces,board
+        for piece in allPieces: piece.clone()
+        if check(i)!="none": check(i).alive=False
         piece.x=i[0]
         piece.y=i[1]
-        if check(i)!=["none"]: check(i).alive=False
         if kingcheck(piece.color): possible.remove(i)
-        declone()
+        elif i[0]<1 or i[0]>board[0] or i[1]<1 or i[1]>board[1]: possible.remove(i)
+        for piece in allPieces: piece.declone()
     return possible
 def possiblePieces(color):
+    global allPieces
     possible=["none"]
     if color=="w":
         for piece in allPieces:
@@ -166,23 +170,28 @@ def possiblePieces(color):
             if checkcheck(piece,move(piece)) and piece.color=="b": possible.append(piece)
     if len(possible)>1:possible.remove("none")
     return possible
-def clone():
-    for piece in allPieces: piece.clone()
-def declone():
-    for piece in allPieces: piece.declone()
 def translateMoveMade(moveMade):
-    if isinstance(moveMade[0],int): moveMade=intToAlphabet(moveMade[0])+","+str(moveMade[1])
-    else: moveMade=[alphabetToInt(moveMade[:moveMade.index(",")+1]),int(moveMade[moveMade.index(","):])]
+    if isinstance(moveMade[0],int): moveMade=intToAlphabet(moveMade[0])+""+str(moveMade[1])
+    else:
+        for char in range(len(moveMade)):
+            if moveMade[char].isdigit(): charindex=char
+        moveMade=[alphabetToInt(moveMade[:charindex]),int(moveMade[charindex:])]
     return moveMade
 def translatePossiblePieces(possiblePieces):
     ret=""
     for piece in range(len(possiblePieces)):
-        ret+=piece.piece+" "
+        ret+=possiblePieces[piece].piece+" "
         for char in possiblePieces[piece].name:
             if char.isdigit(): ret+=char
         ret+=", "
     return ret[:-2]
 def display():
+    global board
+    for y in range(board[1],0,-1):
+        for x in range(1,board[0]+1):
+            if check([x,y])!="none": name=check([x,y]).name
+            else: name="No piece"
+            print(name+" is at "+str(x)+","+str(y))
     pass
     #♔♕♖♗♘♙♚♛♜♝♞♟ 
 def move(piece):
@@ -192,11 +201,11 @@ def move(piece):
         if piece.piece=="Pawn":
             if linearmoveto(piece.x,piece.y,piece.color,False,True,0,1,1,canCapture=False)!=["none"]: possible.extend(linearmoveto(piece.x,piece.y,piece.color,False,True,0,1,1,canCapture=False))
             if linearmoveto(piece.x,piece.y,piece.color,True,True,1,1,1,mustCapture=True)!=["none"]: possible.extend(linearmoveto(piece.x,piece.y,piece.color,True,True,1,1,1,mustCapture=True))
-            if linearmoveto(piece.x,piece.y,piece.color,False,True,0,2,1,canCapture=False)!=["none"] and not piece.hasMoved: possible.extend(linearmoveto(piece.x,piece.y,piece.color,False,True,0,2,1,canCapture=False))
-            if check([piece.x+1,piece.y+colorrev(1)])!="none":
-                if check([piece.x+1,piece.y+colorrev(1)]).color!=piece.color and check([piece.x+1,piece.y+colorrev(1)]).piece=="Pawn" and check([piece.x+1,piece.y+colorrev(1)]).extra: possible.append[(piece.x+1,piece.y+colorrev(1)])
-            if check([piece.x-1,piece.y+colorrev(1)])!="none":
-                if check([piece.x-1,piece.y+colorrev(1)]).color!=piece.color and check([piece.x-1,piece.y+colorrev(1)]).piece=="Pawn" and check([piece.x-1,piece.y+colorrev(1)]).extra: possible.append([piece.x-1,piece.y+colorrev(1)])
+            if linearmoveto(piece.x,piece.y,piece.color,False,True,0,2,1,xmin=2,canCapture=False)!=["none"] and not piece.hasMoved: possible.extend(linearmoveto(piece.x,piece.y,piece.color,False,True,0,2,1,canCapture=False))
+            if check([piece.x+1,piece.y+colorrev(piece.color,1)])!="none":
+                if check([piece.x+1,piece.y+colorrev(piece.color,1)]).color!=piece.color and check([piece.x+1,piece.y+colorrev(piece.color,1)]).piece=="Pawn" and check([piece.x+1,piece.y+colorrev(piece.color,1)]).extra: possible.append([piece.x+1,piece.y+colorrev(piece.color,1)])
+            if check([piece.x-1,piece.y+colorrev(piece.color,1)])!="none":
+                if check([piece.x-1,piece.y+colorrev(piece.color,1)]).color!=piece.color and check([piece.x-1,piece.y+colorrev(piece.color,1)]).piece=="Pawn" and check([piece.x-1,piece.y+colorrev(piece.color,1)]).extra: possible.append([piece.x-1,piece.y+colorrev(piece.color,1)])
         elif piece.piece=="Rook":
             if linearmoveto(piece.x,piece.y,piece.color,True,False)!=["none"]: possible.extend(linearmoveto(piece.x,piece.y,piece.color,True,False))
             if linearmoveto(piece.x,piece.y,piece.color,False,True)!=["none"]: possible.extend(linearmoveto(piece.x,piece.y,piece.color,False,True))
@@ -214,14 +223,15 @@ def move(piece):
             if linearmoveto(piece.x,piece.y,piece.color,False,True,1,1)!=["none"]: possible.extend(linearmoveto(piece.x,piece.y,piece.color,False,True,1,1))
             if linearmoveto(piece.x,piece.y,piece.color,True,True,1,1)!=["none"]: possible.extend(linearmoveto(piece.x,piece.y,piece.color,True,True,1,1))
             if check([board[0],piece.y])!="none":
-                if not piece.hasMoved and moveto(piece.x+1,piece.y,piece.color) and moveto(piece.x+2,piece.y,piece.color) and not check([board[0],piece.y]).hasMoved and linearmoveto(board[0],check([board[0],piece.y]).y,check([board[0],piece.y]).color,True,False,abs(board[0]-piece.x)-1,xmin=abs(board[0]-piece.x)-1)!="none": possible.append(piece.x+2,piece.y)
+                if not piece.hasMoved and moveto(piece.x+1,piece.y,piece.color) and moveto(piece.x+2,piece.y,piece.color) and not check([board[0],piece.y]).hasMoved and linearmoveto(board[0],check([board[0],piece.y]).y,check([board[0],piece.y]).color,True,False,abs(board[0]-piece.x)-1,xmin=abs(board[0]-piece.x)-1)!="none": possible.append([piece.x+2,piece.y])
             if check([1,piece.y])!="none":
-                if not piece.hasMoved and moveto(piece.x-1,piece.y,piece.color) and moveto(piece.x-2,piece.y,piece.color) and not check([1,piece.y]).hasMoved and linearmoveto(1,check([1,piece.y]).y,check([1,piece.y]).color,True,False,abs(1-piece.x)-1,xmin=abs(1-piece.x)-1)!="none": possible.append(piece.x-2,piece.y)
+                if not piece.hasMoved and moveto(piece.x-1,piece.y,piece.color) and moveto(piece.x-2,piece.y,piece.color) and not check([1,piece.y]).hasMoved and linearmoveto(1,check([1,piece.y]).y,check([1,piece.y]).color,True,False,abs(1-piece.x)-1,xmin=abs(1-piece.x)-1)!="none": possible.append([piece.x-2,piece.y])
     possible.remove("You should not see this.")
     return possible
 def reset():
+    global allPieces
     allPieces[0].reset("w","Pawn",1,2)
-    allPieces[2].reset("w","Pawn",2,2)
+    allPieces[1].reset("w","Pawn",2,2)
     allPieces[2].reset("w","Pawn",3,2)
     allPieces[3].reset("w","Pawn",4,2)
     allPieces[4].reset("w","Pawn",5,2)
@@ -231,7 +241,7 @@ def reset():
     allPieces[8].reset("w","Rook",1,1)
     allPieces[9].reset("w","Rook",8,1)
     allPieces[10].reset("w","Knight",2,1)
-    allPieces[11].reset("w","Knight",7,2)
+    allPieces[11].reset("w","Knight",7,1)
     allPieces[12].reset("w","Bishop",3,1)
     allPieces[13].reset("w","Bishop",6,1)
     allPieces[14].reset("w","Queen",4,1)
@@ -259,6 +269,7 @@ while play=="y" or play=="Y":
     color="w"
     reset()
     while True:
+        display()
         if possiblePieces(color)==["none"]:
             if kingcheck(color): print("The",color,"player is in checkmate!")
             else: print("Stalemate!")
@@ -266,15 +277,16 @@ while play=="y" or play=="Y":
             break
         print("You can move the following pieces:",translatePossiblePieces(possiblePieces(color)))
         while True:
+            global allPieces
             pieceMoveMade=color+input("What piece do you want to move? ").strip(" ").lower()
             for piece in allPieces:
                 if pieceMoveMade==piece.name: pieceMoveMade=piece
             if pieceMoveMade!="none":
-                if pieceMoveMade in possiblePieces: break
+                if pieceMoveMade in possiblePieces(color): break
                 else: print("You can't move that piece!")
             else: print("That is not a valid piece!")
         while True:
-            moveMade=translateMoveMade(input("Where do you want to move "+pieceMoveMade+"? "))
+            moveMade=translateMoveMade(input("Where do you want to move "+translatePossiblePieces([pieceMoveMade])+"? "+str(checkcheck(pieceMoveMade.color,move(pieceMoveMade)))+" lmao "+str(move(pieceMoveMade))+" "))
             if moveMade not in move(pieceMoveMade):
                 print("That's not a vlaid move!")
                 continue
@@ -282,14 +294,14 @@ while play=="y" or play=="Y":
                 print("That would put your king in check!")
                 continue
             else: 
-                print(pieceMoveMade,"moved to",translateMoveMade(moveMade))
+                print(translatePossiblePieces([pieceMoveMade]),"moved to",translateMoveMade(moveMade))
                 break
         if pieceMoveMade.piece=="King": pieceMoveMade.tempx=pieceMoveMade.x
         [pieceMoveMade.x,pieceMoveMade.y]=moveMade
         if check(moveMade)!=["none"]: check(moveMade).alive=False
         if pieceMoveMade.piece=="Pawn":
-            if check([pieceMoveMade.x,pieceMoveMade.y-colorrev(1)])!="none":
-                if check([pieceMoveMade.x,pieceMoveMade.y-colorrev(1)]).piece=="Pawn" and check([pieceMoveMade.x,pieceMoveMade.y-colorrev(1)]).extra: check([pieceMoveMade.x,pieceMoveMade.y-colorrev(1)]).alive=False
+            if check([pieceMoveMade.x,pieceMoveMade.y-colorrev(piece.color,1)])!="none":
+                if check([pieceMoveMade.x,pieceMoveMade.y-colorrev(piece.color,1)]).piece=="Pawn" and check([pieceMoveMade.x,pieceMoveMade.y-colorrev(piece.color,1)]).extra: check([pieceMoveMade.x,pieceMoveMade.y-colorrev(piece.color,1)]).alive=False
             if moveMade[1]==board[0]+(colorrev(pieceMoveMade.color,(board[0]-1)/2)-1*((board[0]-1)/2)): pieceMoveMade.piece=input("What do you wan to promote your pawn to?")
             if not pieceMoveMade.hasMoved: pieceMoveMade.extra=True
             else: pieceMoveMade.extra=False
@@ -297,7 +309,6 @@ while play=="y" or play=="Y":
             if pieceMoveMade.tempx-pieceMoveMade.x<0: check(1,pieceMoveMade.y).x=pieceMoveMade.x-1
             elif pieceMoveMade.tempx-pieceMoveMade.x>0: check(board[0],pieceMoveMade.y).x=pieceMoveMade.x+1
         pieceMoveMade.hasMoved=True
-        display()
         color=swap(color,"w","b")
         if kingcheck(color): print("The",color,"player is in check.")
     play=input("Do you want to play again? (Y/N) ")
