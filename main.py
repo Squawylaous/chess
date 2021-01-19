0402b09460c830e6e4804e9ca6f2f177ba30855a
Squawylaous <57704977+Squawylaous@users.noreply.github.com>
2021-01-18 19:58:45 -0600

    almost finished
    im pretty sure that i rid this thing of bugs this time. en passant now works. just gotta add castling

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ class chess:
     hasMoved=False
     alive=True
     extra=False
+    displayName="You're not suppossed to see this."
     tempcolor="You're not suppossed to see this."
     temppiece="You're not suppossed to see this."
     tempx=0
@@ -15,17 +16,20 @@ class chess:
     temphasMoved=False
     tempalive=True
     tempextra=False
-    def reset(self,color,piece,x,y,extra=False):
+    tempdisplayName="You're not suppossed to see this."
+    def reset(self,color,piece,displayName,x,y,alive=True,hasMoved=False,extra=False):
         self.color=color
         self.piece=piece
         self.x=x
         self.y=y
-        self.hasMoved=False
-        self.alive=True
+        self.hasMoved=hasMoved
+        self.alive=alive
         self.extra=extra
+        self.displayName=displayName
         self.clone()
-    def clone(piece): [piece.tempcolor,piece.temppiece,piece.tempx,piece.tempy,piece.tempalive,piece.temphasMoved,piece.tempextra]=[piece.color,piece.piece,piece.x,piece.y,piece.alive,piece.hasMoved,piece.extra]
-    def declone(piece): [piece.color,piece.piece,piece.x,piece.y,piece.alive,piece.hasMoved,piece.extra]=[piece.tempcolor,piece.temppiece,piece.tempx,piece.tempy,piece.tempalive,piece.temphasMoved,piece.tempextra]
+    def clone(piece): [piece.tempcolor,piece.temppiece,piece.tempx,piece.tempy,piece.tempalive,piece.temphasMoved,piece.tempextra,piece.tempdisplayName]=[piece.color,piece.piece,piece.x,piece.y,piece.alive,piece.hasMoved,piece.extra,piece.displayName]
+    def declone(piece): [piece.color,piece.piece,piece.x,piece.y,piece.alive,piece.hasMoved,piece.extra,piece.displayName]=[piece.tempcolor,piece.temppiece,piece.tempx,piece.tempy,piece.tempalive,piece.temphasMoved,piece.tempextra,piece.tempdisplayName]
+def fill(x,y): return str(x)+(" "*(y-len(str(x))))
 def intToAlphabet(x):
     alpha=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
     ret=""
@@ -68,20 +72,20 @@ def check(xy):
 def kingcheck(color):
     global allPieces
     ret=False
-    if color=="b":
-        xy=[allPieces[31].x,allPieces[31].y]
+    if color=="w":
+        xy=[allPieces[0].x,allPieces[0].y]
         for piece in allPieces:
             if xy in move(piece): ret=True
     else:
-        xy=[allPieces[15].x,allPieces[15].y]
+        xy=[allPieces[1].x,allPieces[1].y]
         for piece in allPieces:
             if xy in move(piece): ret=True
     return ret
 def moveto(x,y,color,canCapture=True,mustCapture=False):
-    check=check([x,y])
-    if (check=="none" and not mustCapture): return True
-    elif not isinstance(check,str):
-        if (check.color!=color and canCapture): return True 
+    piece=check([x,y])
+    if (piece=="none" and not mustCapture): return True
+    elif not isinstance(piece,str):
+        if (piece.color!=color and canCapture): return True 
     else: return False
 def linearmoveto(possible,x,y,color,acrossx,acrossy,xlimit=False,ylimit=False,xdirec=0,ydirec=0,xmin=1,ymin=1,xmult=1,ymult=1,canJump=False,canCapture=True,mustCapture=False):
     global board
@@ -189,6 +193,7 @@ def checkcheck(piece,possible):
             if kingcheck(piece.color): possible.remove(i)
             elif i[0]<1 or i[0]>board[0] or i[1]<1 or i[1]>board[1]: possible.remove(i)
         for pieces in allPieces: pieces.declone()
+    if possible==[]: possible="none"
     return possible
 def possiblePieces(color):
     global allPieces
@@ -216,12 +221,13 @@ def translatePossiblePieces(possiblePieces):
         ret+=possiblePieces[piece].piece+" "
         for char in possiblePieces[piece].name:
             if char.isdigit(): ret+=char
-        ret+=", "
+        ret+=" ("+translateMoveMade([possiblePieces[piece].x,possiblePieces[piece].y])+"), "
     return ret[:-2]
 def display():
     global board
-    print("┌───"+"┬───"*(board[0]-1)+"┐")
+    print("   ┌───"+"┬───"*(board[0]-1)+"┐")
     for y in range(board[1],0,-1):
+        print(" ",end=fill(y,2))
         for x in range(1,board[0]+1):
             print(end="│ ")
             piece=check([x,y])
@@ -229,32 +235,39 @@ def display():
                 if piece.alive:
                     if piece.color=="w": color="\033[33m"
                     else: color="\033[31m"
-                    if piece.piece=="Pawn": name="p"
-                    elif piece.piece=="Knight": name="k"
-                    elif piece.piece=="Rook": name="R"
-                    elif piece.piece=="Bishop": name="B"
-                    elif piece.piece=="Queen": name="Q"
-                    elif piece.piece=="King": name="K"
-                    name=color+name
+                    name=color+piece.displayName
                 else: name=" "
             else: name=" "
             print(name,end="\033[m")
             print(end=" ")
         print("│")
-        if y!=1: print("├───"+"┼───"*(board[0]-1)+"┤")
+        if y!=1: print("   ├───"+"┼───"*(board[0]-1)+"┤")
         else: continue
-    print("└───"+"┴───"*(board[0]-1)+"┘")
+    print("   └───"+"┴───"*(board[0]-1)+"┘")
+    print(end="  ")
+    for x in range(1,board[0]+1): print("   ",end=intToAlphabet(x))
+    print("")
 def move(piece):
     global board
     possible=[]
     if piece.alive:
-        if piece.piece=="Pawn":
+        if piece.piece=="King":
+            possible=linearmoveto(possible,piece.x,piece.y,piece.color,True,False,1,1)
+            possible=linearmoveto(possible,piece.x,piece.y,piece.color,False,True,1,1)
+            possible=linearmoveto(possible,piece.x,piece.y,piece.color,True,True,1,1)
+        elif piece.piece=="Pawn":
             possible=linearmoveto(possible,piece.x,piece.y,piece.color,False,True,0,1,ydirec=1,canCapture=False)
-            possible=linearmoveto(possible,piece.x,piece.y,piece.color,True,True,1,1,ydirec=1,mustCapture=True)
             if not piece.hasMoved: possible=linearmoveto(possible,piece.x,piece.y,piece.color,False,True,0,2,ydirec=1,ymin=2,canCapture=False)
+            possible=linearmoveto(possible,piece.x,piece.y,piece.color,True,True,1,1,ydirec=1,mustCapture=True)
+            temp=check([piece.x+1,piece.y])
+            if temp!="none":
+                if temp.piece=="Pawn" and temp.color!=piece.color and temp.extra and moveto(piece.x+1,piece.y+colorrev(piece.color,1),piece.color,False): possible.append([piece.x+1,piece.y+colorrev(piece.color,1)])
+            temp=check([piece.x-1,piece.y])
+            if temp!="none":
+                if temp.piece=="Pawn" and temp.color!=piece.color and temp.extra and moveto(piece.x-1,piece.y+colorrev(piece.color,1),piece.color,False): possible.append([piece.x-1,piece.y+colorrev(piece.color,1)])
         elif piece.piece=="Rook":
             possible=linearmoveto(possible,piece.x,piece.y,piece.color,True,False)
-            possible=linearmoveto(possible,piece.x,piece.y,piece.color,False,True))
+            possible=linearmoveto(possible,piece.x,piece.y,piece.color,False,True)
         elif piece.piece=="Knight":
             possible=linearmoveto(possible,piece.x,piece.y,piece.color,True,True,2,1,xmult=2)
             possible=linearmoveto(possible,piece.x,piece.y,piece.color,True,True,1,2,ymult=2)
@@ -264,87 +277,92 @@ def move(piece):
             possible=linearmoveto(possible,piece.x,piece.y,piece.color,True,False)
             possible=linearmoveto(possible,piece.x,piece.y,piece.color,False,True)
             possible=linearmoveto(possible,piece.x,piece.y,piece.color,True,True)
-        elif piece.piece=="King":
-            possible=linearmoveto(possible,piece.x,piece.y,piece.color,True,False,1,1)
-            possible=linearmoveto(possible,piece.x,piece.y,piece.color,False,True,1,1)
-            possible=linearmoveto(possible,piece.x,piece.y,piece.color,True,True,1,1)
     return possible
 def reset():
     global allPieces
-    allPieces[0].reset("w","Pawn",1,2)
-    allPieces[1].reset("w","Pawn",2,2)
-    allPieces[2].reset("w","Pawn",3,2)
-    allPieces[3].reset("w","Pawn",4,2)
-    allPieces[4].reset("w","Pawn",5,2)
-    allPieces[5].reset("w","Pawn",6,2)
-    allPieces[6].reset("w","Pawn",7,2)
-    allPieces[7].reset("w","Pawn",8,2)
-    allPieces[8].reset("w","Rook",1,1)
-    allPieces[9].reset("w","Rook",8,1)
-    allPieces[10].reset("w","Knight",2,1)
-    allPieces[11].reset("w","Knight",7,1)
-    allPieces[12].reset("w","Bishop",3,1)
-    allPieces[13].reset("w","Bishop",6,1)
-    allPieces[14].reset("w","Queen",4,1)
-    allPieces[15].reset("w","King",5,1)
-    allPieces[16].reset("b","Pawn",8,7)
-    allPieces[17].reset("b","Pawn",7,7)
-    allPieces[18].reset("b","Pawn",6,7)
-    allPieces[19].reset("b","Pawn",5,7)
-    allPieces[20].reset("b","Pawn",4,7)
-    allPieces[21].reset("b","Pawn",3,7)
-    allPieces[22].reset("b","Pawn",2,7)
-    allPieces[23].reset("b","Pawn",1,7)
-    allPieces[24].reset("b","Rook",8,8)
-    allPieces[25].reset("b","Rook",1,8)
-    allPieces[26].reset("b","Knight",7,8)
-    allPieces[27].reset("b","Knight",2,8)
-    allPieces[28].reset("b","Bishop",6,8)
-    allPieces[29].reset("b","Bishop",3,8)
-    allPieces[30].reset("b","Queen",4,8)
-    allPieces[31].reset("b","King",5,8)
-#┌───┬───┬───┬───┬───┬───┬───┬───┐
-#│ R │ k │ B │ Q │ K │ B │ k │ R │
-#├───┼───┼───┼───┼───┼───┼───┼───┤
-#│ p │ p │ p │ p │ p │ p │ p │ p │
-#├───┼───┼───┼───┼───┼───┼───┼───┤
-#│   │   │   │   │   │   │   │   │
-#├───┼───┼───┼───┼───┼───┼───┼───┤
-#│   │   │   │   │   │   │   │   │
-#├───┼───┼───┼───┼───┼───┼───┼───┤
-#│   │   │   │   │   │   │   │   │
-#├───┼───┼───┼───┼───┼───┼───┼───┤
-#│   │   │   │   │   │   │   │   │
-#├───┼───┼───┼───┼───┼───┼───┼───┤
-#│ p │ p │ p │ p │ p │ p │ p │ p │
-#├───┼───┼───┼───┼───┼───┼───┼───┤
-#│ R │ k │ B │ Q │ K │ B │ k │ R │
-#└───┴───┴───┴───┴───┴───┴───┴───┘
-allPieces=[chess("wpawn1"),chess("wpawn2"),chess("wpawn3"),chess("wpawn4"),chess("wpawn5"),chess("wpawn6"),chess("wpawn7"),chess("wpawn8"),chess("wrook1"),chess("wrook2"),chess("wknight1"),chess("wknight2"),chess("wbishop1"),chess("wbishop2"),chess("wqueen1"),chess("wking1"),chess("bpawn1"),chess("bpawn2"),chess("bpawn3"),chess("bpawn4"),chess("bpawn5"),chess("bpawn6"),chess("bpawn7"),chess("bpawn8"),chess("brook1"),chess("brook2"),chess("bknight1"),chess("bknight2"),chess("bbishop1"),chess("bbishop2"),chess("bqueen1"),chess("bking1")]
+    allPieces[0].reset("w","King","K",5,1)
+    allPieces[1].reset("b","King","K",5,8)
+    allPieces[2].reset("w","Pawn","p",1,2)
+    allPieces[3].reset("w","Pawn","p",2,2)
+    allPieces[4].reset("w","Pawn","p",3,2)
+    allPieces[5].reset("w","Pawn","p",4,2)
+    allPieces[6].reset("w","Pawn","p",5,2)
+    allPieces[7].reset("w","Pawn","p",6,2)
+    allPieces[8].reset("w","Pawn","p",7,2)
+    allPieces[9].reset("w","Pawn","p",8,2)
+    allPieces[10].reset("w","Rook","R",1,1)
+    allPieces[11].reset("w","Rook","R",8,1)
+    allPieces[12].reset("w","Knight","N",2,1)
+    allPieces[13].reset("w","Knight","N",7,1)
+    allPieces[14].reset("w","Bishop","B",3,1)
+    allPieces[15].reset("w","Bishop","B",6,1)
+    allPieces[16].reset("w","Queen","Q",4,1)
+    allPieces[17].reset("b","Pawn","p",8,7)
+    allPieces[18].reset("b","Pawn","p",7,7)
+    allPieces[19].reset("b","Pawn","p",6,7)
+    allPieces[20].reset("b","Pawn","p",5,7)
+    allPieces[21].reset("b","Pawn","p",4,7)
+    allPieces[22].reset("b","Pawn","p",3,7)
+    allPieces[23].reset("b","Pawn","p",2,7)
+    allPieces[24].reset("b","Pawn","p",1,7)
+    allPieces[25].reset("b","Rook","R",8,8)
+    allPieces[26].reset("b","Rook","R",1,8)
+    allPieces[27].reset("b","Knight","N",7,8)
+    allPieces[28].reset("b","Knight","N",2,8)
+    allPieces[29].reset("b","Bishop","B",6,8)
+    allPieces[30].reset("b","Bishop","B",3,8)
+    allPieces[31].reset("b","Queen","Q",4,8)
+#   ┌───┬───┬───┬───┬───┬───┬───┬───┐
+# 8 │ R │ k │ B │ Q │ K │ B │ k │ R │
+#   ├───┼───┼───┼───┼───┼───┼───┼───┤
+# 7 │ p │ p │ p │ p │ p │ p │ p │ p │
+#   ├───┼───┼───┼───┼───┼───┼───┼───┤
+# 6 │   │   │   │   │   │   │   │   │
+#   ├───┼───┼───┼───┼───┼───┼───┼───┤
+# 5 │   │   │   │   │   │   │   │   │
+#   ├───┼───┼───┼───┼───┼───┼───┼───┤
+# 4 │   │   │   │   │   │   │   │   │
+#   ├───┼───┼───┼───┼───┼───┼───┼───┤
+# 3 │   │   │   │   │   │   │   │   │
+#   ├───┼───┼───┼───┼───┼───┼───┼───┤
+# 2 │ p │ p │ p │ p │ p │ p │ p │ p │
+#   ├───┼───┼───┼───┼───┼───┼───┼───┤
+# 1 │ R │ k │ B │ Q │ K │ B │ k │ R │
+#   └───┴───┴───┴───┴───┴───┴───┴───┘
+#     A   B   C   D   E   F   G   H
+allPieces=[chess("wking1"),chess("bking1"),chess("wpawn1"),chess("wpawn2"),chess("wpawn3"),chess("wpawn4"),chess("wpawn5"),chess("wpawn6"),chess("wpawn7"),chess("wpawn8"),chess("wrook1"),chess("wrook2"),chess("wknight1"),chess("wknight2"),chess("wbishop1"),chess("wbishop2"),chess("wqueen1"),chess("bpawn1"),chess("bpawn2"),chess("bpawn3"),chess("bpawn4"),chess("bpawn5"),chess("bpawn6"),chess("bpawn7"),chess("bpawn8"),chess("brook1"),chess("brook2"),chess("bknight1"),chess("bknight2"),chess("bbishop1"),chess("bbishop2"),chess("bqueen1")]   
 board=[8,8]
 play="y"
 while play=="y" or play=="Y":
     color="w"
     reset()
     while True:
+        if color=="w": displayColor="White"
+        else: displayColor="Black"
         display()
         if possiblePieces(color)==["none"]:
-            if kingcheck(color): print("The",color,"player is in checkmate!")
+            if kingcheck(color): print("The",displayColor,"player is in checkmate!")
             else: print("Stalemate!")
-            print("The",swap(color,"w","b"),"player wins!")
+            print("The",swap(displayColor,"White","Black"),"player wins!")
             break
-        print("Player",color,"can move the following pieces:",translatePossiblePieces(possiblePieces(color)))
+        print("The",displayColor,"player can move the following pieces:",translatePossiblePieces(possiblePieces(color)))
         while True:
-            pieceMoveMade=color+input("What piece do you want to move? ").strip(" ").lower()
+            pieceMoveMade=color+(input("What piece do you want to move? ").replace(" ","")).lower()
             for piece in allPieces:
-                if pieceMoveMade==piece.name: pieceMoveMade=piece
+                if pieceMoveMade==piece.name:
+                    pieceMoveMade=piece
+                    break
+                else: pieceMoveMade="none"
             if pieceMoveMade!="none":
                 if pieceMoveMade in possiblePieces(color): break
                 else: print("You can't move that piece!")
             else: print("That is not a valid piece!")
         while True:
             pieceMoveMadeMoves=move(pieceMoveMade)
-            moveMade=translateMoveMade(input("Where do you want to move "+translatePossiblePieces([pieceMoveMade])+"? "+str(checkcheck(pieceMoveMade,pieceMoveMadeMoves))+" "))
+            try: moveMade=translateMoveMade(input("Where do you want to move "+translatePossiblePieces([pieceMoveMade])+"? ").upper())
+            except:
+                print("That's not a vlaid move!")
+                continue
             if moveMade not in pieceMoveMadeMoves:
                 print("That's not a vlaid move!")
                 continue
@@ -354,16 +372,30 @@ while play=="y" or play=="Y":
             else: 
                 print(translatePossiblePieces([pieceMoveMade]),"moved to",translateMoveMade(moveMade))
                 break
-        if pieceMoveMade.piece=="King": pieceMoveMade.tempx=pieceMoveMade.x
+#for castling        if pieceMoveMade.piece=="King": pieceMoveMade.tempx=pieceMoveMade.x
         if check(moveMade)!="none": check(moveMade).alive=False
         [pieceMoveMade.x,pieceMoveMade.y]=moveMade
+        if pieceMoveMade.piece=="Pawn":
+            if moveMade[1]==board[0]+(colorrev(pieceMoveMade.color,(board[0]-1)/2)-((board[0]-1)/2)):
+                pawnPromote=input("What do you want to promote your pawn to?").capatlize()
+                while pawnPromote not in ["Queen","Rook","Bishop","Knight"]:
+                    print("That's not a valid piece!")
+                    pawnPromote=input("What do you want to promote your pawn to?").capatlize()
+                count=1
+                for piece in allPieces:
+                    if piece.piece==pawnPromote: count+=1
+                pieceMoveMade.piece=pawnPromote
+                pieceMoveMade.name=pieceMoveMade.color+pawnPromote.lower()+str(count)
+                if pawnPromote=="Knight": pieceMoveMade.displayName="N"
+                else: pieceMoveMade.displayName=pawnPromote[0]
+            enPassant=check([pieceMoveMade.x,pieceMoveMade.y-colorrev(pieceMoveMade.color,1)])
+            if enPassant!="none":
+                if enPassant.extra and enPassant.color!=pieceMoveMade.color: enPassant.alive=False
         for piece in allPieces:
             if piece.piece=="Pawn": pieceMoveMade.extra=False
-        if pieceMoveMade.piece=="Pawn":
-            if moveMade[1]==board[0]+(colorrev(pieceMoveMade.color,(board[0]-1)/2)-((board[0]-1)/2)): pieceMoveMade.piece=input("What do you wan to promote your pawn to?")
-            if not pieceMoveMade.hasMoved: pieceMoveMade.extra=True
+        if not pieceMoveMade.hasMoved and pieceMoveMade.piece=="Pawn": pieceMoveMade.extra=True
         pieceMoveMade.hasMoved=True
         color=swap(color,"w","b")
-        if kingcheck(color): print("The",color,"player is in check.")
+        if kingcheck(color): print("The",swap(displayColor,"White","Black"),"player is in check.")
     play=input("Do you want to play again? (Y/N) ")
 print("Goodbye!")
